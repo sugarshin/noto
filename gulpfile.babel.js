1ce41eefcda6dbb12bee426d70ceff8d9a78a644
sugarshin <shinsugar@gmail.com>
2015-08-01 11:57:59 +0900

    edit gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -30,12 +30,21 @@ gulp.task('default', ['predefault'], () => {
   );
 });
 
-gulp.task('build', cb => {
+gulp.task('build:html', cb => {
+  runSequence('jade', 'replace', cb);
+});
+
+gulp.task('build:css', cb => {
+  runSequence('stylus', 'minify-css', cb);
+});
+
+gulp.task('build:js', cb => {
+  runSequence('browserify', 'uglify', cb);
+});
+
+gulp.task('build', ['clean'], cb => {
   runSequence(
-    'clean',
-    ['jade', 'stylus'],
-    ['replace', 'minify-css', 'browserify'],
-    'uglify',
+    ['build:html', 'build:css', 'build:js'],
     cb
   );
 });
