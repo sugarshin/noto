f9b75d3132199a264cfad89eda29a6c245ad1a79
sugarshin <shinsugar@gmail.com>
2015-08-25 01:20:24 +0900

    refactor trashed-notes.jsx

--- src/js/components/trashed-notes.jsx
+++ src/js/components/trashed-notes.jsx
@@ -8,14 +8,7 @@ export default class TrashedNotes extends Component {
 
   static get propTypes() {
     return {
-      notes: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        title: PropTypes.string,
-        body: PropTypes.string,
-        createdAt: PropTypes.string,
-        trashed: PropTypes.bool,
-        tags: PropTypes.arrayOf(PropTypes.string)
-      }))
+      notes: PropTypes.arrayOf(PropTypes.object)
     };
   }
 
