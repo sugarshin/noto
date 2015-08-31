a993087f35771828903d0afd310eefc4b24aaeed
sugarshin <shinsugar@gmail.com>
2015-08-31 14:41:17 +0900

    WIP tests note-stores

--- src/js/__test__/stores/note-store.js
+++ src/js/__test__/stores/note-store.js
@@ -1,7 +1,10 @@
 import assert from 'power-assert';
+import assign from 'object-assign';
 
 import { noteStore } from '../../context';
 import { DEFAULT } from '../../constants/constants';
+
+const DEFAULT_NOTE = DEFAULT.note;
 
 describe('NoteStore', () => {
   beforeEach(done => {
@@ -12,10 +15,10 @@ describe('NoteStore', () => {
   describe('_fetchNotes()', () => {
     it('case 1', () => {
       const _assert = () => {
-        assert.deepEqual(noteStore.getNotes(), [DEFAULT.note]);
+        assert.deepEqual(noteStore.getNotes(), [DEFAULT_NOTE]);
       };
       noteStore.addChangeListener(_assert);
-      noteStore._fetchNotes([DEFAULT.note]);
+      noteStore._fetchNotes([DEFAULT_NOTE]);
       noteStore._emitChange();
       noteStore.removeChangeListener(_assert);
     });
@@ -24,10 +27,49 @@ describe('NoteStore', () => {
   describe('_createNote()', () => {
     it('case 1', () => {
       const _assert = () => {
-        assert.deepEqual(noteStore.getNotes(), [DEFAULT.note]);
+        assert.deepEqual(noteStore.getNotes(), [DEFAULT_NOTE]);
       };
       noteStore.addChangeListener(_assert);
-      noteStore._createNote(DEFAULT.note);
+      noteStore._createNote(DEFAULT_NOTE);
+      noteStore._emitChange();
+      noteStore.removeChangeListener(_assert);
+    });
+  });
+
+  describe('_trashNote()', () => {
+    it('case 1', () => {
+      const _id = Date.now();
+      noteStore._notes = [assign({}, DEFAULT_NOTE, {id: _id})];
+
+      const _assert = () => {
+        assert.ok(noteStore.getNotes()[0].trashed);
+      };
+      noteStore.addChangeListener(_assert);
+      noteStore._trashNote(_id);
+      noteStore._emitChange();
+      noteStore.removeChangeListener(_assert);
+    });
+  });
+
+  describe('_trashCheckedNote()', () => {
+    it('case 1', () => {
+      const _ids = [
+        123, 234, 345
+      ];
+      noteStore._notes = [
+        assign({}, DEFAULT_NOTE, {id: _ids[0]}),
+        assign({}, DEFAULT_NOTE, {id: _ids[1]}),
+        assign({}, DEFAULT_NOTE, {id: _ids[2]})
+      ];
+
+      const _assert = () => {
+        const notes = noteStore.getNotes();
+        assert(notes[0].trashed === true);
+        assert(notes[1].trashed === false);
+        assert(notes[2].trashed === true);
+      };
+      noteStore.addChangeListener(_assert);
+      noteStore._trashCheckedNote([_ids[0], _ids[2]]);
       noteStore._emitChange();
       noteStore.removeChangeListener(_assert);
     });
