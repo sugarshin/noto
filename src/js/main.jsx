43686340b1ebd05a6ad5d57356e1887bb20207ca
sato <sato@tsumikiinc.com>
2015-09-28 18:12:07 +0900

    update main.jsx

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -7,7 +7,7 @@ import routes from './config/routes';
 import { APP_ELEMENT_ID } from './constants/constants';
 
 global.Promise = global.Promise || Promise;
-global.regeneratorRuntime = regeneratorRuntime;
+global.regeneratorRuntime = global.regeneratorRuntime || regeneratorRuntime;
 
 run(routes, HashLocation, Root => {
   React.render(<Root />, document.getElementById(APP_ELEMENT_ID));
